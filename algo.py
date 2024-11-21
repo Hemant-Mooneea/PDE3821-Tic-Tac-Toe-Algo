6577ad210c9ca32728f085b93dac8f7cae10eb03
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-11-21 07:59:23 +0400

    add grid initialisation and can print grid in a more readable format

--- algo.py
+++ algo.py
@@ -1,5 +1,18 @@
 import random
 
+
+def getEmptyGrid():
+    # Return a 3x3 grid with all empty strings
+    return [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ]
+def printGrid(grid):
+    # Print the grid in a readable format
+    for row in grid:
+        print(row)
+        
 def getShape():
     # If random is 0, then return 'X', else return 'O'
     if(random.randint(0,1) == 0):
@@ -8,7 +21,7 @@ def getShape():
     
 def main():
     BOT_SHAPE = getShape()
-    print(BOT_SHAPE)
+    playingGrid = getEmptyGrid()
     
 main()
     
