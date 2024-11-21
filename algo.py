9e67bfdd07dbd2ae992d105a0e98054d653f6b4b
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-11-21 08:47:56 +0400

    refactor currentTurn so that it holds string instead of 1s and 0s for more clarity

--- algo.py
+++ algo.py
@@ -1,7 +1,6 @@
 import random
 # Global variable to keep track of the current turn
-# 1 for player, 0 for bot
-currentTurn = 0
+currentTurn = "BOT"
 
 def checkWin(grid):
     # checking rows
@@ -25,13 +24,13 @@ def getPlayerMove(grid):
     # import machine learning file la apres fer li geter kotsa player la in zuer
     # e.g machine learning pou return position dan grid kt player la pu met so move
     print("hello world")
-    currentTurn = 0
+    currentTurn = "BOT"
     
 def getBotMove(grid, shape):
     print("hello world")
 
 def gameLogic(grid, shape):
-    if (currentTurn == 1):
+    if (currentTurn == "PLAYER"):
         getPlayerMove(grid)
         checkWin(grid)
     else:
@@ -56,9 +55,9 @@ def getShape():
     # If random is 0, then return 'X', else return 'O'
     if(random.randint(0,1) == 0):
         return 'X'
-        currentTurn = 1
+        currentTurn = "PLAYER"
         
-    currentTurn = 0  
+    currentTurn = "BOT"
     return 'O'
     
 def main():
