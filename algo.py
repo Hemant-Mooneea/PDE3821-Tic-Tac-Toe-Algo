f393e620b693b2bd37cb729a97d590f5aca2433e
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-11-21 08:25:04 +0400

    add currentTurn global variable to keep track of whose turn it is to play

--- algo.py
+++ algo.py
@@ -1,6 +1,16 @@
 import random
+# Global variable to keep track of the current turn
+# 1 for player, 0 for bot
+currentTurn = 0
 
+def gameLogic(grid, shape):
+    print("hello world")
 
+def getPlayerMove(grid):
+    # import machine learning file la apres fer li geter kotsa player la in zuer
+    # e.g machine learning pou return position dan grid kt player la pu met so move
+    print("hello world")
+    
 def getEmptyGrid():
     # Return a 3x3 grid with all empty strings
     return [
@@ -17,11 +27,15 @@ def getShape():
     # If random is 0, then return 'X', else return 'O'
     if(random.randint(0,1) == 0):
         return 'X'
+        currentTurn = 0
+        
+    currentTurn = 1   
     return 'O'
     
 def main():
     BOT_SHAPE = getShape()
     grid = getEmptyGrid()
+    gameLogic(grid, BOT_SHAPE)
     
 main()
     
