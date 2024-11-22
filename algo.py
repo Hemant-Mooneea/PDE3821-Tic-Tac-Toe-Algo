10ce9289e24ce5e19477812b4e68759957db6874
K_Nund <ON144@live.mdx.ac.uk>
2024-11-22 14:31:57 +0400

    -Separates grid detection and arm movement functions in their respective files - Adds the findBestMove() function to algo.py

--- algo.py
+++ algo.py
@@ -1,73 +1,28 @@
 import random
+# Global variable to keep track of the current turn
+# TODO : camera.py which returns a position(eg. [0, 1],[1, 2] etc) which the player players a move
+# TODO : YOLOV8 or alternatives for object detection
+# TODO : game logic for the bot which will try to win, block player, random move(adjacent to another shape) in that order
+# TODO : varying the difficulty of the bot by adding probability of moves
 
-#SECTION: Detecting grid cells
+currentTurn = ""
 
-
-#? using opencv to capture frame of grid
-
-#! add opencv code here
-
-
-
-#* use yolo model or whatever to scan grid cells, separate them using bounding boxes and label them as coordinates 
-#* such as (0,0), (0,1), etc and determine whether its empty, filled with X or O
-
-
-#! add code here for detection
-
-
-
-
-""" Each of those coordinates will then be assigned a value depending on what they contain
-# a dictionary could be used to store these key-value pairs as such: (0,0):0 (empty), (0,1):1 (filled with X)
-? 0: empty grid cell
-? 1: represents X
-? 2: represents O
-"""
-
-#example of said dictionary
-coordinateValueDict = {
-    (0, 0): 0,  
-    (0, 1): 1,  
-    (0, 2): 2, 
-    (1, 0): 0,
-    (1, 1): 2,
-    (1, 2): 1,
-    (2, 0): 0,
-    (2, 1): 0,
-    (2, 2): 1,
-    #etc
-}
-
-#*default starting grid, will get updated as game proceeds
-grid = [[0,0,0],
-        [0,0,0],
-        [0,0,0]]
-
-#* updating the grid as per the dictionary
-for (row,col), value in coordinateValueDict.items():
-    grid[row][col] = value
-    
-
-#SECTION: Checking if player has won
-
-def checkWin(grid, player):
-    
+def checkWin(grid):
+    # checking rows
     for i in range(3):
-        #checking rows
-        if all(row == player for row in grid[i]):
+        if ((grid[i][0] == grid[i][1] == grid[i][2]) and grid[i][0] != ""):
             return True
-        #checking columns
-        if all(column[i] == player for column in grid):
+    # checking columns
+    for i in range(3):
+        if ((grid[0][i] == grid[1][i] == grid[2][i]) and grid[0][i] != ""):
             return True
-
-    #checking diagonal from top to bottom
-    if grid[0][0] == grid[1][1] == grid[2][2] == player:
-        return True
-        #checking diagonal from bottom to top
-    if grid[0][2] == grid[1][1] == grid[2][0] == player:
+    # checking diagonals
+    if ((grid[0][0] == grid[1][1] == grid[2][2]) and grid[0][0] != ""):
         return True
     
+    if ((grid[0][2] == grid[1][1] == grid[2][0]) and grid[0][2] != ""):
+        return True
+    # return False if no win
     return False
 
 
@@ -102,43 +57,53 @@ def findBestMove(grid):
             if grid[i][j] == 0:
                 normalMoves.append([i,j])
 
-
-#SECTION: choosing the move and playing it
-
-#?choose a random move from the good moves array, if no good moves available
-#?then choose a random move from the normal moves array
-
-#to be randomly generated
-indexChosen = random.randint(0,2)
-move_chosen = goodMoves[indexChosen]
-
-
-#! Mapping coordinates to robot arm servo angles
-#! Assume we've already manually calibrated angles for each cell
-
-#maybe we could manually moves the arm to the center of each grid and record the angles used?
-#idk just speculating but i think it could be done
-
-# Lookup table: (row, col) -> Servo angles
-grid_to_servo_angles = {
-    (0, 0): [30, 45, 60, 90, 0, 0],  # Angles for top-left cell
-    (0, 1): [35, 50, 65, 95, 0, 0],  # Angles for top-middle cell
-    (0, 2): [40, 55, 70, 100, 0, 0], # Angles for top-right cell
-    # etc
-}
-
-# Function to move the arm to a cell
-def move_to_cell(row, col):
-    angles = grid_to_servo_angles[(row, col)]  # Get angles for the cell
-    for i, angle in enumerate(angles):
-        # Command each servo to the angle
-        print(f"Moving servo {i + 1} to {angle}°")
-        # Add actual Yahboom SDK servo movement code here
-        # e.g., dofbot.set_servo_angle(i + 1, angle)
-
-# Example move: Robot chooses (1, 2)
-move_to_cell(move_chosen[0], move_chosen[1])
-
-
+def getPlayerMove(grid):
+    # import machine learning file la apres fer li geter kotsa player la in zuer
+    # e.g machine learning pou return position dan grid kt player la pu met so move
+    print("hello world")
+    currentTurn = "BOT"
+    
+def getBotMove(grid, shape):
+    print("hello world")
+    currentTurn = "PLAYER"
+    
+def gameLogic(grid, shape):
+    if (currentTurn == "PLAYER"):
+        getPlayerMove(grid)
+        checkWin(grid)
+    else:
+        getBotMove(grid, shape)
+        checkWin(grid)
         
 
+def getEmptyGrid():
+    # Return a 3x3 grid with all empty strings
+    return [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ]
+
+def printGrid(grid):
+    # Print the grid in a readable format
+    for row in grid:
+        print(row)
+        
+def getShape():
+    # If random is 0, then return 'X', else return 'O'
+    if(random.randint(0,1) == 0):
+        return 'X'
+        currentTurn = "PLAYER"
+        
+    currentTurn = "BOT"
+    return 'O'
+    
+def main():
+    BOT_SHAPE = getShape()
+    grid = getEmptyGrid()
+    
+    while(True):
+        gameLogic(grid, BOT_SHAPE)
+    
+main()
+    
