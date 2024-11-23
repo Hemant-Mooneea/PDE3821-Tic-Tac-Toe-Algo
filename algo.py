efd4a81ceddbfff7e4b7133c2348e2697651e510
K_Nund <ON144@live.mdx.ac.uk>
2024-11-23 14:39:55 +0400

    -Adds player parameter to checkWin -Adds robotPlay() function to do repeat main set of actions each time -Turns camera.py and arm.py logic into function components for better readability -Tries to link algo, camera and arm.py

--- algo.py
+++ algo.py
@@ -1,37 +1,30 @@
 import random
+import camera
+import arm
 # Global variable to keep track of the current turn
 # TODO : camera.py which returns a position(eg. [0, 1],[1, 2] etc) which the player players a move
 # TODO : YOLOV8 or alternatives for object detection
 # TODO : game logic for the bot which will try to win, block player, random move(adjacent to another shape) in that order
 # TODO : varying the difficulty of the bot by adding probability of moves
 
-currentTurn = ""
-
-def checkWin(grid):
-    # checking rows
+#* simulates winning and blocking moves and saves those moves to their respective arrays
+def checkWin(grid, player):
     for i in range(3):
-        if ((grid[i][0] == grid[i][1] == grid[i][2]) and grid[i][0] != ""):
+        if all(rowCell == player for rowCell in grid[i]):  # Check rows/horizontally, grid[i] represents a row
             return True
-    # checking columns
-    for i in range(3):
-        if ((grid[0][i] == grid[1][i] == grid[2][i]) and grid[0][i] != ""):
+        if all(row[i] == player for row in grid):  # Check columns/vertically, check all columns at index 0,1,2
             return True
-    # checking diagonals
-    if ((grid[0][0] == grid[1][1] == grid[2][2]) and grid[0][0] != ""):
+    if grid[0][0] == grid[1][1] == grid[2][2] == player:  # Check diagonal from left top to right bottom
         return True
-    
-    if ((grid[0][2] == grid[1][1] == grid[2][0]) and grid[0][2] != ""):
+    if grid[0][2] == grid[1][1] == grid[2][0] == player:  # Check diagonal from left bottom to right top
         return True
-    # return False if no win
+    #return False if no win
     return False
 
-
-
-#SECTION: Calculating different moves and the best moves
-normalMoves = [] #all moves that can be played
-goodMoves = [] #moves that are likely going to secure a win or block opponent's win
-
+#* Calculating different moves and the best moves
 def findBestMove(grid):
+    #? robotSide and playerSide can be 1 or 2 depending on which side they chose
+    #? 1 for X, 2 for O
     
     #find the best possible moves
     for i in range(3):
@@ -40,14 +33,14 @@ def findBestMove(grid):
             if grid[i][j] == 0:
 
                 #simulate playing X and see if it is a winning move
-                grid[i][j] = 1
-                if checkWin(grid,1):
-                    goodMoves.append([i,j])
+                grid[i][j] = robotSide
+                if checkWin(grid,robotSide):
+                    winningMoves.append([i,j])
                    
                 #simulate playing O and see if it is a winning move for the opponent
-                grid[i][j] = 2
-                if checkWin(grid,2):
-                    goodMoves.append([i,j])
+                grid[i][j] = playerSide
+                if checkWin(grid,playerSide):
+                    blockingMoves.append([i,j])
 
                 #reset grid cell after simulation
                 grid[i][j] = 0
@@ -57,54 +50,61 @@ def findBestMove(grid):
         for j in range(3):
             if grid[i][j] == 0:
                 normalMoves.append([i,j])
-                
-def getPlayerMove(grid):
-    # import machine learning file la apres fer li geter kotsa player la in zuer
-    # e.g machine learning pou return position dan grid kt player la pu met so move
-    print("hello world")
-    currentTurn = "BOT"
-    
-def getBotMove(grid, shape):
-    print("hello world")
-    currentTurn = "PLAYER"
-    
-def gameLogic(grid, shape):
-    if (currentTurn == "PLAYER"):
-        getPlayerMove(grid)
-        checkWin(grid)
-    else:
-        getBotMove(grid, shape)
-        checkWin(grid)
-        
 
-def getEmptyGrid():
-    # Return a 3x3 grid with all empty strings
-    return [
-        ["", "", ""],
-        ["", "", ""],
-        ["", "", ""]
-    ]
-  
-def printGrid(grid):
-    # Print the grid in a readable format
-    for row in grid:
-        print(row)
-        
-def getShape():
-    # If random is 0, then return 'X', else return 'O'
-    if(random.randint(0,1) == 0):
-        return 'X'
-        currentTurn = "PLAYER"
-        
-    currentTurn = "BOT"
-    return 'O'
+#* captures grid each time, updates gridArray and make arm play the chosen move at its coordinates        
+def robotPlay():
+    gridArray = camera.captureGridAndConvertToArray()
+    findBestMove(gridArray)
+
+    if (len(winningMoves) != 0):
+        chosenMove = random.choice(winningMoves)
+        arm.playThisMove(chosenMove[0],chosenMove[1])
+        #! after a winning move is played, game is basically over
+        #! so we could stop the process here right? idk how to do it
+        #! you figure it out ig
+
+    elif (len(blockingMoves) !=0):
+        chosenMove = random.choice(blockingMoves)
+        arm.playThisMove(chosenMove[0],chosenMove[1])
+    else:
+        chosenMove = random.choice(normalMoves)
+        arm.playThisMove(chosenMove[0],chosenMove[1])
+
+#SECTION: Main code
+#! had to remove the main() cause i wasn't able to access my robotSide variable inside the if function 
+#? PS: how about we turn this main code into a tkinter app? then each time, it's the robot's turn,
+#? PS: we press a button to let it know it's its turn
+
+winningMoves = [] #moves that wins the game
+blockingMoves = [] #moves that blocks opponent's winning moves
+normalMoves = [] #basic random moves if no winning or blocking moves available
+currentTurn = ""
+gameOn = True
+robotSide = 0
+
+#! this is just a draft
+print("Welcome!\n")
+
+#! there could probably be start button in the tkinter app
+#! and these would be shown after pressing start
+
+playerSide = int(input("What side do you want to play on? \n1.X \n2.O \nChoice: "))
+
+if (playerSide == 1):
+    robotSide = 2
+    currentTurn = "player"
+else:
+    robotSide = 1
+    currentTurn = "robot"
+    robotPlay()
+
+#! then after the robot has played, the player will play right
+#! and after he finishes, he clicks on another button called "Robot's turn" or idk
+#! and it re-executes robotPlay() each time it's clicked
+
+#* press button
+    robotPlay()
     
-def main():
-    BOT_SHAPE = getShape()
-    grid = getEmptyGrid()
+
     
-    while(True):
-        gameLogic(grid, BOT_SHAPE)
-    
-main()
     
--- camera.py
+++ camera.py
@@ -1,49 +1,46 @@
 
-#! moon if you seeing this, it's just a draft for me, relax, take a chill pill
-#SECTION: Detecting grid cells
+#SECTION: Detecting grid cells and extracting details
+
+def captureGridAndConvertToArray():
+
+    #? using opencv to capture frame of grid
+    #! add opencv code here
 
 
-#? using opencv to capture frame of grid
+    #? use yolo model or whatever to scan grid cells, separate them using bounding boxes and label them as coordinates 
+    #? such as (0,0), (0,1), etc and determine whether its empty, filled with X or O
 
-#! add opencv code here
-
-
-
-#* use yolo model or whatever to scan grid cells, separate them using bounding boxes and label them as coordinates 
-#* such as (0,0), (0,1), etc and determine whether its empty, filled with X or O
+    #! add code here for detection
 
 
-#! add code here for detection
+    """ Each of those coordinates will then be assigned a value depending on what they contain
+    # a dictionary could be used to store these key-value pairs as such: (0,0):0 (empty), (0,1):1 (filled with X)
+    ? 0: empty grid cell
+    ? 1: represents X
+    ? 2: represents O
+    """
 
+    #example of said dictionary
+    coordinateValueDict = {
+        (0, 0): 0,  
+        (0, 1): 1,  
+        (0, 2): 2, 
+        (1, 0): 0,
+        (1, 1): 2,
+        (1, 2): 1,
+        (2, 0): 0,
+        (2, 1): 0,
+        (2, 2): 1,
+        #etc
+    }
 
+    #*default starting grid, will get updated as game proceeds
+    grid = [[0,0,0],
+            [0,0,0],
+            [0,0,0]]
 
+    #* updating the grid as per the dictionary
+    for (row,col), value in coordinateValueDict.items():
+        grid[row][col] = value
 
-""" Each of those coordinates will then be assigned a value depending on what they contain
-# a dictionary could be used to store these key-value pairs as such: (0,0):0 (empty), (0,1):1 (filled with X)
-? 0: empty grid cell
-? 1: represents X
-? 2: represents O
-"""
-
-#example of said dictionary
-coordinateValueDict = {
-    (0, 0): 0,  
-    (0, 1): 1,  
-    (0, 2): 2, 
-    (1, 0): 0,
-    (1, 1): 2,
-    (1, 2): 1,
-    (2, 0): 0,
-    (2, 1): 0,
-    (2, 2): 1,
-    #etc
-}
-
-#*default starting grid, will get updated as game proceeds
-grid = [[0,0,0],
-        [0,0,0],
-        [0,0,0]]
-
-#* updating the grid as per the dictionary
-for (row,col), value in coordinateValueDict.items():
-    grid[row][col] = value
+    return grid
