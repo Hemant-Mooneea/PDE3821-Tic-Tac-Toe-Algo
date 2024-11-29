232bc263f2c352487f48ab4e63e6e24530a40c5d
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-11-29 11:40:10 +0400

    rework arm.py into a class

--- arm.py
+++ arm.py
@@ -1,33 +1,22 @@
-
-#SECTION: choosing the move and playing it
-
-def moveArmToCell(dofbot,coordinateX, coordinateY):
-
-    #! Mapping coordinates to robot arm servo angles
-    #! Assume we've already manually calibrated angles for each cell
-
-    # Lookup table: (row, col) -> Servo angles
-    coordinate_to_servo_angles = {
-        (0, 0): [30, 45, 60, 90, 0, 0],  # Angles for top-left cell
-        (0, 1): [35, 50, 65, 95, 0, 0],  # Angles for top-middle cell
-        (0, 2): [40, 55, 70, 100, 0, 0], # Angles for top-right cell
-        # etc
-    }
-  
-    try:
-        # Get angles for the cell
-        angles = coordinate_to_servo_angles[(coordinateX, coordinateY)]
-        for i, angle in enumerate(angles):
-            print(f"Moving servo {i + 1} to {angle}°")
-
-            # Move each servo to the specified angle
-            dofbot.set_servo_angle(i + 1, angle)  
-
-    except KeyError:
-
-        print("Invalid grid coordinates. Please check the lookup table.")
-
-    except Exception as e:
+from Arm_Lib import Arm_Device
+class Arm:
+    def __init__(self):
+        self.arm = Arm_Device()
         
-        print(f"An error occurred while moving the arm: {e}")
-
+        delay = 500
+        self.grid_to_angles = {
+            1: [0, 0, 0, 0, 0, 0, delay],
+            2: [0, 0, 0, 0, 0, 0, delay],
+            3: [0, 0, 0, 0, 0, 0, delay],
+            4: [0, 0, 0, 0, 0, 0, delay],
+            5: [0, 0, 0, 0, 0, 0, delay],
+            6: [0, 0, 0, 0, 0, 0, delay],
+            7: [0, 0, 0, 0, 0, 0, delay],
+            8: [0, 0, 0, 0, 0, 0, delay],
+            9: [0, 0, 0, 0, 0, 0, delay]
+        }
+        
+    def moveToGrid(self, pos):    
+        angles = self.grid_to_angles[pos]
+        # Use unpacking to pass the values to Arm_serial_servo_write6
+        self.arm.Arm_serial_servo_write6(*angles)
