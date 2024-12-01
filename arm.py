66fa825187047429b511a8b6b73835762a7ddf69
K_Nund <ON144@live.mdx.ac.uk>
2024-12-01 16:38:14 +0400

    -Changes positions from numbers to coordinates as moves are in coordinates -Adds watch position for arm while camera is looking at grid

--- arm.py
+++ arm.py
@@ -5,18 +5,22 @@ class Arm:
         
         delay = 500
         self.grid_to_angles = {
-            1: [0, 0, 0, 0, 0, 0, delay],
-            2: [0, 0, 0, 0, 0, 0, delay],
-            3: [0, 0, 0, 0, 0, 0, delay],
-            4: [0, 0, 0, 0, 0, 0, delay],
-            5: [0, 0, 0, 0, 0, 0, delay],
-            6: [0, 0, 0, 0, 0, 0, delay],
-            7: [0, 0, 0, 0, 0, 0, delay],
-            8: [0, 0, 0, 0, 0, 0, delay],
-            9: [0, 0, 0, 0, 0, 0, delay]
+            (0,0): [0, 0, 0, 0, 0, 0, delay],
+            (0,1): [0, 0, 0, 0, 0, 0, delay],
+            (0,2): [0, 0, 0, 0, 0, 0, delay],
+            (1,0): [0, 0, 0, 0, 0, 0, delay],
+            (1,1): [0, 0, 0, 0, 0, 0, delay],
+            (1,2): [0, 0, 0, 0, 0, 0, delay],
+            (2,0): [0, 0, 0, 0, 0, 0, delay],
+            (2,1): [0, 0, 0, 0, 0, 0, delay],
+            (2,2): [0, 0, 0, 0, 0, 0, delay]
         }
         
     def moveToGrid(self, pos):    
         angles = self.grid_to_angles[pos]
         # Use unpacking to pass the values to Arm_serial_servo_write6
         self.arm.Arm_serial_servo_write6(*angles)
+
+    def moveToWatchPosition(self):
+        self.arm.Arm_serial_servo_write6("//enter angles for watch grid position")
+
