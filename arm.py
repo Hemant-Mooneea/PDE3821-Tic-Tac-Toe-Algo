ccfd8f7f97ec46d65c3d68a34e3791ad8dc6991e
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-12-02 12:22:41 +0400

    hard code angles

--- arm.py
+++ arm.py
@@ -1,19 +1,21 @@
 from Arm_Lib import Arm_Device
+import time
 class Arm:
     def __init__(self):
         self.arm = Arm_Device()
         
-        self.delay = 500
+        self.delay = 2500
+        self.moveDelay = 1500
         self.grid_to_angles = {
-            (0,0): [0, 0, 0, 0, 0, 0, self.delay],
-            (0,1): [0, 0, 0, 0, 0, 0, self.delay],
-            (0,2): [0, 0, 0, 0, 0, 0, self.delay],
-            (1,0): [0, 0, 0, 0, 0, 0, self.delay],
-            (1,1): [0, 0, 0, 0, 0, 0, self.delay],
-            (1,2): [0, 0, 0, 0, 0, 0, self.delay],
-            (2,0): [96, 115, 0, 14, 89, 180, self.delay],
-            (2,1): [87,114, 0, 14, 89, 180, self.delay],
-            (2,2): [80, 114, 0, 15, 89, 180, self.delay]
+            (0,0): [94, 106, 0, 24, 89, 180, self.moveDelay],
+            (0,1): [87, 106, 0, 24, 89, 180, self.moveDelay],
+            (0,2): [82, 106, 0, 24, 89, 180, self.moveDelay],
+            (1,0): [95, 113, 0, 14, 89, 180, self.moveDelay],
+            (1,1): [87, 113, 0, 14, 89, 180, self.moveDelay],
+            (1,2): [81, 113, 0, 14, 89, 180, self.moveDelay],
+            (2,0): [96, 120, 0, 5, 89, 180, self.moveDelay],
+            (2,1): [87,120, 0, 6, 89, 180, self.moveDelay],
+            (2,2): [80, 120, 0, 6, 89, 180, self.moveDelay]
         }
         
     def moveToGrid(self, pos):    
@@ -29,3 +31,13 @@ class Arm:
     
     def insertPen(self):
         self.arm.Arm_serial_servo_write6(88,116,0,13,89,180,self.delay)
+
+testarm = Arm()
+
+testarm.moveToGrid((0,2))
+time.sleep(1.5)
+testarm.moveToRestPosition()
+time.sleep(1.5)
+testarm.insertPen()
+time.sleep(5.5)
+testarm.moveToRestPosition()
