39ca3893b8395dbf9350138acacfb6c05ccfc39c
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-12-07 17:10:25 +0400

    add delay to moveToRest and moveToWatch to account for time taken for arm to move

--- arm.py
+++ arm.py
@@ -3,19 +3,17 @@ import time
 class Arm:
     def __init__(self):
         self.arm = Arm_Device()
-        
-        self.delay = 2500
-        self.moveDelay = 1500
+        self.delay = 1500
         self.grid_to_angles = {
-            (0,2): [95, 85, 0, 10, 90, 180, self.moveDelay],
-            (0,1): [86, 85, 0, 10, 90, 180, self.moveDelay],
-            (0,0): [79, 85, 0, 10, 90, 180, self.moveDelay],
-            (1,2): [95, 82, 0, 18, 90, 180, self.moveDelay],
-            (1,1): [86, 82, 0, 18, 90, 180, self.moveDelay],
-            (1,0): [79, 82, 0, 18, 90, 180, self.moveDelay],
-            (2,2): [94.5, 78, 0, 26, 90, 180, self.moveDelay],
-            (2,1): [87.5, 78, 0, 26, 90, 180, self.moveDelay],
-            (2,0): [80.5, 78, 0, 26, 90, 180, self.moveDelay]
+            (0,2): [95, 85, 0, 10, 90, 180, self.delay],
+            (0,1): [86, 85, 0, 10, 90, 180, self.delay],
+            (0,0): [79, 85, 0, 10, 90, 180, self.delay],
+            (1,2): [95, 82, 0, 18, 90, 180, self.delay],
+            (1,1): [86, 82, 0, 18, 90, 180, self.delay],
+            (1,0): [79, 82, 0, 18, 90, 180, self.delay],
+            (2,2): [94.5, 78, 0, 26, 90, 180, self.delay],
+            (2,1): [87.5, 78, 0, 26, 90, 180, self.delay],
+            (2,0): [80.5, 78, 0, 26, 90, 180, self.delay]
         }
         
     def moveToGrid(self, pos):    
@@ -24,13 +22,14 @@ class Arm:
         self.arm.Arm_serial_servo_write6(*angles)
         time.sleep(1.5)
         self.moveToRestPosition()
-        time.sleep(1.5)
 
     def moveToWatchPosition(self):
         self.arm.Arm_serial_servo_write6(90,95,0,0,90,180,self.delay)
+        time.sleep(1.5)
         
     def moveToRestPosition(self):
-        self.arm.Arm_serial_servo_write6(90,180,0,0,90,180,self.moveDelay)
+        self.arm.Arm_serial_servo_write6(90,180,0,0,90,180,self.delay)
+        time.sleep(1.5)
           
     def testAllGrids(self):
         for i in range(3):
