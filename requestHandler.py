087e7a0488b49049812f2abf4e66c10b2125f397
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-12-12 14:19:40 +0400

    comment request Handler

--- requestHandler.py
+++ requestHandler.py
@@ -1,32 +1,40 @@
 import requests
 import time
-
 class requestHandler:
     def __init__(self):
+        # URL of the server which is localhost in this case
         self.url = "http://127.0.0.1:5000"
+    # Method which sends a GET request to the server to get the shapes selected by the player
     def getShapes(self):
         while True:
             try:
+                # Send a GET request to the server
                 response = requests.get(f"{self.url}/shape")
+                # Get the response in JSON format
                 data = response.json()
+                # Check if the response is successful and the player has selected a shape
                 if response.status_code == 200 and data['player_shape'] != "":
                     print("Got Shapes")
                     return data['bot_shape'], data['player_shape'], data['current_turn']
+                # If the player has not selected a shape, wait for 2 seconds and try again
                 print("Waiting for player to select shape")
             except Exception as e:
                 print(f"failed to get shape: {e}")
             time.sleep(2)
-            
+    # Method which sends a DELETE request to the server to reset the shapes when the game ends
     def resetShapes(self):
         try:
+            # Send a DELETE request to the server
             response = requests.delete(f"{self.url}/shape")
         except Exception as e:
             print(f"failed to delete shape: {e}")
-    
+    # Method which sends a GET request to the server to get the last played shape
     def getLastPlayed(self):
         while True:
             try:
+                # Send a GET request to the server
                 response = requests.get(f"{self.url}/last-played")
+                # Get the response in JSON format
                 data = response.json()
                 return data['last_played']
             except Exception as e:
